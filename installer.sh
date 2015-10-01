8027939cf5280004eb3295e514e871f0fd41c7b4
suiyujin <suiyujin04@gmail.com>
2015-10-01 19:30:03 +0900

    fix indent and style

--- installer.sh
+++ installer.sh
@@ -2,11 +2,9 @@
 cd $(dirname $0)
 for dotfile in .?*
 do
-    if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != '.gitignore' ] && [ $dotfile != '.DS_Store' ]
-    then
-      if [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ] && [ $dotfile != '.Brewfile' ]
-      then
-        ln -is "$PWD/$dotfile" $HOME
-      fi
+  if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != '.gitignore' ] && [ $dotfile != '.DS_Store' ]; then
+    if [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ] && [ $dotfile != '.Brewfile' ]; then
+      ln -is "$PWD/$dotfile" $HOME
     fi
+  fi
 done
