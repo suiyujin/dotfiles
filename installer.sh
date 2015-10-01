aabe7e1dbe0e633be38e8f454eeb35560ad2b0f0
suiyujin <suiyujin04@gmail.com>
2015-10-01 20:03:29 +0900

    change sh to bash and fix script

--- installer.sh
+++ installer.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 cd $(dirname $0)
 for dotfile in .?*
 do
-  if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != '.gitignore' ] && [ $dotfile != '.DS_Store' ]; then
-    if [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ] && [ $dotfile != '.Brewfile' ]; then
-      ln -is "$PWD/$dotfile" $HOME
-    fi
+  if [ "$dotfile" == '..' ] || [ "$dotfile" == '.git' ] || [ "$dotfile" == '.gitignore' ] || [ "$dotfile" == '.DS_Store' ]; then
+    echo "not link : " $dotfile
+    continue
+  elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ] && [ "$dotfile" == '.Brewfile' ]; then
+    echo "not link : " $dotfile
+    continue
   fi
+  ln -is "$PWD/$dotfile" $HOME
 done
