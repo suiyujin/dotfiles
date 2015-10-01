606728b10d0415310b1e1d078b47bbd0727e45ae
suiyujin <suiyujin04@gmail.com>
2015-10-01 19:29:13 +0900

    not link .Brewfile in linux

--- installer.sh
+++ installer.sh
@@ -4,6 +4,9 @@ for dotfile in .?*
 do
     if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != '.gitignore' ] && [ $dotfile != '.DS_Store' ]
     then
+      if [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ] && [ $dotfile != '.Brewfile' ]
+      then
         ln -is "$PWD/$dotfile" $HOME
+      fi
     fi
 done
